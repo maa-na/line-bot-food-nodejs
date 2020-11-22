1e1ecfca108b80f442d109e65d9fb521b5040d23
maa-na <git config --global user.email>
2020-11-23 01:37:29 +0900

    wip

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import path from 'path'
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  // .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  // .get('/', (req, res) => res.render('pages/index'))
   .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"}))
   .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"}))
   .post("/hook/", (req, res) => res.json({ test: "hook" }))// 追加
