d1749d9c426d2badac794985f3fd5694fc561614
maa-na <git config --global user.email>
2020-11-23 14:22:43 +0900

    return text edit

--- index.mjs
+++ index.mjs
@@ -17,32 +17,24 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')) //ブラウザ確認用(無くても問題ない)
 app.get("/webhook", (req, res) => res.send('webhook ok(GET)')) // 追加
 app.post("/webhook", (req, res) => res.json({ test: "hook" })) // 追加
-app.post('/webhook', line.middleware(config), (req, res) => {
-    console.log(req.body.events);
-
-    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('疎通確認用');
-        return; 
-    }
-
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result));
-});
+app.post('/webhook', line.middleware(config), (req, res) => handleEvent(req, res))
 
 const client = new line.Client(config);
 
-async function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
+async function handleEvent(req, res) {
+  const events = req.body.events;
+  const promises = events.map(event => replay(event))
+
+  Promise.all(promises).then(console.log("pass"));
+}
+
+async function replay(event) {
+  const profile =  await client.getProfile(event.source.userId);
 
   return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text //実際に返信の言葉を入れる箇所
-  });
+    type: "text",
+    text: `${profile.displayName}さん、今「${event.message.text}」って言いました？`
+  })
 }
 
 app.listen(PORT);
