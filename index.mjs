f89800f5b48f9281773ba5dc0553f2917775e951
maa-na <git config --global user.email>
2020-11-23 23:21:21 +0900

    rich menu return fix

--- index.mjs
+++ index.mjs
@@ -52,16 +52,14 @@ async function textCheck(event) {
       returnText = '入力された時間から１時間半以上滞在できるお店を紹介します。時間を入力してください。(今から、２１時、１５分後　など)'
       break
   }
-  console.log('returnText', returnText)
+
   if (!returnText) return false 
+  
   Promise.all(client.replyMessage(event.replyToken, {
     type: "text",
     text: returnText
-  }))
-    .then(() => {
-      console.log("first pass ok")
-      return true
-    });
+  })).then(console.log("first pass ok"));
+
   return true
 }
 
