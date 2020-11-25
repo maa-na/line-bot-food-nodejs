2faf729d65e811c6d6ed23e6959b448a018920a7
maa-na <git config --global user.email>
2020-11-25 23:01:19 +0900

    console add

--- index.mjs
+++ index.mjs
@@ -28,7 +28,8 @@ async function handleEvent(req, res) {
 
   if (event.message.latitude) {
     console.log('位置情報送られた')
-    console.log('event1つ前の', req.body.events[1])
+    console.log('event1つ前の', req.body.events)
+    return
   }
 
   const textCheckRes = await textCheck(event)
